7d93d9645ddf7b05efead6923570029f5cca135e
jack <229376483@qq.com>
2018-06-09 19:29:42 +0800

    Initial Commit

---
+++ DataStruct/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStruct
+//
+//  Created by 胡勇 on 2018/6/9.
+//  Copyright © 2018年 hu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
