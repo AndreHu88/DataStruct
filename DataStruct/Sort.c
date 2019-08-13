b5acc4e933f8d6debda6cedb84cd93e52e90af38
Jack Hu <229376483@qq.com>
2019-08-13 14:52:30 +0800

    add search Algorithm

---
+++ DataStruct/Sort.c
@@ -0,0 +1,12 @@
+//
+//  Sort.c
+//  DataStruct
+//
+//  Created by Jack on 2019/8/13.
+//  Copyright © 2019 hu. All rights reserved.
+//
+
+#include "Sort.h"
+
+
+
---
+++ DataStruct/Sort.h
@@ -0,0 +1,14 @@
+//
+//  Sort.h
+//  DataStruct
+//
+//  Created by Jack on 2019/8/13.
+//  Copyright © 2019 hu. All rights reserved.
+//
+
+#ifndef Sort_h
+#define Sort_h
+
+#include <stdio.h>
+
+#endif /* Sort_h */
